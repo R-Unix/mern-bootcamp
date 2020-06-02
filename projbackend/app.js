6afae9b3772324ec789e66dca4981e07fc3c75f3
IshanJawade <ishankjawade@gmail.com>
2020-06-02 15:30:10 +0530

    Middleware added in app.js

--- projbackend/app.js
+++ projbackend/app.js
@@ -3,17 +3,32 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+// DB connections
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
 }).then(() =>{
-    console.log("Database is connected successfully");
+    console.log("Database is connected successfully !");
 });
 
-const port = 8000;
+// Middelwares 
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
+// My Routes
+app.use("/")
+
+
+// PORT INFO
+const port = process.env.PORT || 8000;
+
+// Starting a Server
 app.listen(port, () => {
-    console.log(`app is running at ${port}`);
+    console.log(`app is running at port number ${port} ...`);
 })
