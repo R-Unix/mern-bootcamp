0343b133bee9f0594a0e414b7d2c9081e7117375
IshanJawade <ishankjawade@gmail.com>
2020-06-21 18:25:56 +0530

    Errors are managed properly with mgs

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var router = express.Router();
 const { signout, signup } = require("../controllers/auth");
+const { check, validationResult } = require('express-validator');
 
 
-router.post("/signup", signup)
+router.post("/signup",[
+    check("name").isLength({ min: 5 }).withMessage('name should be at least 3 characters'),
+    check("email").isEmail().withMessage('email is required'),
+    check("password").isLength({ min:3 }).withMessage('password should be 8 characters long'),
+], signup);
+
 router.get("/signout", signout);
 
 module.exports = router;
