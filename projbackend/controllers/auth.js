cb7411531b75cc46aac521bf1468651a4ab07040
IshanJawade <ishankjawade@gmail.com>
2020-07-05 22:49:55 +0530

    Sent token in cookie for user

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,8 +1,8 @@
 const User = require("../models/user");
 const { check, validationResult } = require('express-validator');
 const user = require("../models/user");
-var jwt = required('jsonwebtoken');
-var expressJwt = required('express-jwt');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
 exports.signup = (req, res) => {
 
@@ -32,34 +32,34 @@ exports.signup = (req, res) => {
 exports.signin = (req, res) => {
     const {email, password} = req.body;  //this will extract email and password from request body
     
-    if(!errors.isEmpty()){
+    if(!errors.isEmpty()){                  // Validation part Error responce
         return res.status(422).json({
             error: errors.array()[0].msg
         });
     }
 
-    User.findOne({email}, (err, user)=>{
-        if(err){
+    User.findOne({email}, (err, user)=> {                //find one is function of mongo db
+        if (err) {
             res.status(400).json({
                 error: "User email does not exists in records",
             })
         }
-        if(!user.autheticate(password)){
+        if (!user.autheticate(password)) {
             return res.status(401).json({
                 error: "Email and password do not match",
             })
         }
-    })
 
-    //create token
-    const token = jwt.sign({_id: user._id}, process.env.SECRET);
-    //put token in cookie
-    res.cookie("token", token, {expire: new Date() + 9999});
 
-    // send res to front end
-    const {_id, name, email, role} = user; // deconstruction of user
-    return res.json({token, user: {_id, name, email, role}});
+        //create token
+        const token = jwt.sign({_id: user._id}, process.env.SECRET);
+        //put token in cookie
+        res.cookie("token", token, {expire: new Date() + 9999});
 
+        // send res to front end
+        const {_id, name, email, role} = user; // deconstruction of user
+        return res.json({token, user: {_id, name, email, role}});
+    });
 };
 
 exports.signout = (req, res) => {
